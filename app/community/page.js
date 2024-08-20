7428d927d2b9e5012f6f98260e4f82a08aa6512d
Gustavo Bedin <gutoobedin@gmail.com>
2024-08-20 14:54:15 -0300

    Adicionando algumas pages

---
+++ app/community/page.js
@@ -0,0 +1,7 @@
+export default function CommunityPage() {
+  return (
+    <main>
+      <h1 style={{ color: "white", textAlign: "center" }}>COMMUNITY PAGE</h1>
+    </main>
+  );
+}
---
+++ app/meals/[mealSlug]/page.js
@@ -0,0 +1,7 @@
+export default function MealDetailPage() {
+  return (
+    <main>
+      <h1 style={{ color: "white", textAlign: "center" }}>MEAL DETAILS</h1>
+    </main>
+  );
+}
